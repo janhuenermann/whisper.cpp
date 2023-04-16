f5aeb1315a18e060fc7f9c46186c45f2c1e83bc3
Jan Hünermann <j.huenermann@icloud.com>
2023-04-16 16:00:58 +0100

    install dynamic library in proper place

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,38 +1,38 @@
 from glob import glob
 import os
 import subprocess
+import sys
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext as _build_ext
+from sysconfig import get_path
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 WHISPER_ENABLE_COREML = False
 
 
-def get_library_paths():
-    return glob(os.path.join("build", "libwhisper.*"))
+def run_cmake():
+    # Run CMake to build the libwhisper library
+    cmake_dir = os.path.realpath(os.path.join("..", ".."))
+    pkg_build_dir = os.path.realpath("build")
+    os.makedirs(pkg_build_dir, exist_ok=True)
+
+    extra_cmake_flags = []
+    if WHISPER_ENABLE_COREML:
+        extra_cmake_flags.append("-DWHISPER_COREML=1")
+
+    subprocess.check_call(["cmake", "-DCMAKE_BUILD_TYPE=Release", *extra_cmake_flags, "-Wno-dev", cmake_dir], cwd=pkg_build_dir)
+    subprocess.check_call(["cmake", "--build", ".", "--target", "whisper"], cwd=pkg_build_dir)
 
 
-class build_ext(_build_ext):
-    def run(self):
-        # Run CMake to build the libwhisper library
-        cmake_dir = os.path.realpath(os.path.join("..", ".."))
-        pkg_build_dir = os.path.realpath("build")
-        os.makedirs(pkg_build_dir, exist_ok=True)
+run_cmake()
 
-        extra_cmake_flags = []
-        if WHISPER_ENABLE_COREML:
-            extra_cmake_flags.append("-DWHISPER_COREML=1")
-
-        subprocess.check_call(["cmake", "-DCMAKE_BUILD_TYPE=Release", *extra_cmake_flags, "-Wno-dev", cmake_dir], cwd=pkg_build_dir)
-        subprocess.check_call(["cmake", "--build", ".", "--target", "whisper"], cwd=pkg_build_dir)
-        super().run()
-
-
+lib_path = get_path("platlib")
 ext_modules = [
     Pybind11Extension(
         "pywhisper",
         ["pybind.cpp"],
         include_dirs=[".", "../.."],
         extra_link_args=["-Lbuild", "-lwhisper"],
+        runtime_library_dirs=[get_path("platlib")]
     ),
 ]
 
@@ -45,5 +45,5 @@ setup(
     license="MIT",
     cmdclass={"build_ext": build_ext},
     ext_modules=ext_modules,
-    data_files=[("lib", get_library_paths())]
+    data_files=[(os.path.relpath(lib_path, sys.prefix), glob(os.path.join("build", "libwhisper.*")))],
 )
